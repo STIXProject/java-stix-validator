0fcffd3687865bf58b0a32e6a60840e33c284198
nemonik <github.com@nemonik.com>
2015-05-20 15:57:52 -0400

    birth

---
+++ src/main/java/org/mitre/stix/validator/App.java
@@ -0,0 +1,23 @@
+package org.mitre.stix.validator;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.jade.JadeTemplateEngine;
+
+public class App {
+	private static final int PORT = System.getenv("OPENSHIFT_DIY_IP") != null ? Integer.parseInt(System.getenv("OPENSHIFT_DIY_IP")) : Integer.parseInt(System.getProperty("port"));
+
+	public static void main(String[] args) {
+		
+		port(PORT);
+		
+		Map<String, String> map = new HashMap<>();
+		map.put("message", "Hello World!");
+		get("/", (req, res) -> new ModelAndView(map, "hello"), new JadeTemplateEngine());
+	}
+}
